305d732e19eb99290ad0fc86637ac9050b6172eb
Kovacs Benjamin Mark ADV D EU HU OPS 2 MPS 5 <benjamin.kovacs@evosoft.com>
2022-05-16 10:11:13 +0200

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000,function(){
-    console.log("server is up and running.")
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Kovacs'}
+]
+
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice:)',
+        users: mockUserData
+    })
 })
+
+app.listen(8000,function(){console.log('server is up and running.')})
